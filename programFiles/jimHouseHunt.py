d03d4d0103f7ca92ffa731566b47ad1c64bfb308
darwady2 <darwady2@gmail.com>
2017-10-09 21:32:56 -0700

    new client secret; using gspread client

--- programFiles/jimHouseHunt.py
+++ programFiles/jimHouseHunt.py
@@ -35,51 +35,9 @@ import smtplib
 from email.mime.text import MIMEText
 
 from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
 
 
 
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'jimHouseHunt'
-
-
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
 
 
 
@@ -132,44 +90,10 @@ def matches_to_list(matches):
 	return entries
 
 
-def authorize_sheets():
-	credentials = get_credentials()
-	http = credentials.authorize(httplib2.Http())
-	discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?' 'version=v4')
-	service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)
-	return service
-
-
 #Iterates through each home and places it in a row in sheets.
 def matches_to_sheets(matches):
-	
-
-	service = authorize_sheets()
-	spreadsheetId = '1Ne-S5Nuzu5NrxoZV6S9ejf5oN3OU4fdadDn6oObeMAo' #Testing House Hunt Spreadsheet, Dan's Google Drive.
 	entries = matches_to_list(matches)
-	rangeName = 'Sheet1'
-	value = entries[0][0]
-	value_range_body = {"range":rangeName,"values":[["test"]]}
-	result = service.spreadsheets().values().append(
-		spreadsheetId = spreadsheetId, range = rangeName, body=value_range_body).execute()
-	
-	
-	  
-	"""
-    rangeName = 'Class Data!A2:E'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
-
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
-	"""
-
+	return entries
 
 def main():
 
